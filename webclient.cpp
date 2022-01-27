9489ef60bb68f5ed9aa94ba21ec4583f66cf9ae7
NicoNii03 <91891479+NicoNii03@users.noreply.github.com>
2022-01-27 22:52:45 +0100

    Add files via upload

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,39 @@
 #include "webclient.h"
+#include <QCoreApplication>
+#include <QDebug>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::readyRead);
+
+
 }
+
+void Webclient::on_pushButton_clicked()
+{
+    textFeld->clear();                                                              // Das Text Feld muss nach jedem Durchlauf gelcleared werden
+    m_hostname = Webseite->text();                                                  // Die Variable nimmt den Text der TextBox an
+
+    m_socket->connectToHost(m_hostname, m_zahl);
+    if (!m_socket->waitForConnected(6000)) {                                        // Wenn nach 6 Sekunden keine Verbindung zum Server ist dann wird im Text Feld no Connection ausgegeben
+        textFeld->setText("No Connectiion");
+    }
+}
+
+void Webclient::connected()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_hostname + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::readyRead()
+{
+    textFeld->setText( m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+
--- webclient.h
+++ webclient.h
@@ -1,5 +1,7 @@
 #ifndef WEBCLIENT_H
 #define WEBCLIENT_H
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 #include "ui_webclient.h"
 
@@ -9,6 +11,15 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    QTcpSocket *m_socket;
+    QString m_hostname;
+    unsigned short m_zahl = 80;
+    void readyRead();
+    void connected();
 };
 
 #endif // WEBCLIENT_H
